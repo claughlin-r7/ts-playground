2b006507283e6850bf9c6f6cbbf8fe7eb8118268
Chris Laughlin <chris_laughlin@rapid7.com>
2022-07-27 10:48:08 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+Typescript Playground 
+
+## Run the playground
+
+`npm run dev`
+
+Then edit `index.ts`
---
+++ index.ts
@@ -0,0 +1,11 @@
+const person: string = 'chris'
+
+const list:Array<number> = [1, 2, 3, 4,]
+
+console.log(person);
+
+console.log(list)
+
+const someone = 'chris';
+
+console.log(someone);
